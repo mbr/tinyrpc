d3de9a566a7e18c5b574ee333fa1b323228ebdb3
Marc Brinkmann <git@marcbrinkmann.de>
2013-01-31 01:44:43 +0100

    Simplified and un-threaded http module.

--- tinyrpc/transports/http.py
+++ tinyrpc/transports/http.py
@@ -13,41 +13,20 @@ class HttpPostClientTransport(ClientTransport):
         self.endpoint = endpoint
         self.request_kwargs = kwargs
 
-        self.request_queue = Queue()
-        self.reply_queue = Queue()
-
-        self._background_thread = threading.Thread(target=self._run_background)
-        self._background_thread.daemon = True
-        self._background_thread.start()
-
-    def __del__(self):
-        self.request_queue.put(None)
-        self._background_thread.join()
-        super(HttpPostClientTransport, self).__del__()
-
     def _run_background(self):
         while True:
             message = self.request_queue.get()
             if message == None:
                 break
 
-            try:
-                r = requests.post(self.endpoint, data=message, **self.request_kwargs)
-            except Exception as e:
-                self.reply_queue.put(e)
-            else:
-                self.reply_queue.put(r)
-
     def send_message(self, message):
         if not isinstance(message, str):
             raise TypeError('str expected')
 
-        self.request_queue.put(message)
+        self._response = requests.post(self.endpoint, data=message, **self.request_kwargs)
+
 
     def receive_reply(self):
-        reply = self.request_queue.get()
+        assert self._response
 
-        if isinstance(reply, Exception):
-            raise reply
-
-        return reply
+        return self._reponse.data
