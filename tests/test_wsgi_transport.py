98466615e9800aa2dbfe4d4d47e54353cab3a4df
Marc Brinkmann <git@marcbrinkmann.de>
2013-01-31 01:57:00 +0100

    Added wsgi transport test (some).

---
+++ tests/test_wsgi_transport.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import pytest
+
+import gevent
+import gevent.queue
+import gevent.monkey
+from gevent.pywsgi import WSGIServer
+import requests
+
+from tinyrpc.transports.wsgi import WsgiServerTransport
+
+TEST_SERVER_ADDR = ('127.0.0.1', 49294)
+
+
+@pytest.fixture(scope='module', autouse=True)
+def monkey_patches(request):
+    # ugh? ugh. ugh. ugh!
+    import socket
+    import httplib
+
+    gevent.monkey.patch_all(
+        socket=True,
+        dns=False,
+        time=False,
+        select=False,
+        thread=False,
+        os=False,
+        ssl=False,
+        httplib=True,
+        aggressive=False,
+    )
+
+    def fin():
+        reload(socket)
+        reload(httplib)
+
+    request.addfinalizer(fin)
+
+
+@pytest.fixture()
+def wsgi_server(request):
+    app = WsgiServerTransport(queue_class=gevent.queue.Queue)
+
+    server = WSGIServer(TEST_SERVER_ADDR, app.handle)
+
+    def fin():
+        server.stop()
+        server_greenlet.join()
+
+    request.addfinalizer(fin)
+    server_greenlet = gevent.spawn(server.serve_forever)
+    gevent.sleep(0)  # wait for server to come up
+
+    return (app, 'http://%s:%d' % TEST_SERVER_ADDR)
+
+
+def test_server_supports_post_only(wsgi_server):
+    transport, addr = wsgi_server
+
+    r = requests.get(addr)
+
+    # we expect a "not supported" response
+    assert r.status_code == 405
+
+    r = requests.head(addr)
+
+    # we expect a "not supported" response
+    assert r.status_code == 405
+
+
+@pytest.mark.parametrize(('msg',),
+    [('foo',), ('',), ('bar',), ('1234',), ('{}',), ('{',), ('\x00\r\n',)])
+def test_server_receives_messages(wsgi_server, msg):
+    transport, addr = wsgi_server
+
+    def consumer():
+        context, received_msg = transport.receive_message()
+        assert received_msg == msg
+        reply = 'reply:' + msg
+        transport.send_reply(context, reply)
+
+    gevent.spawn(consumer)
+
+    r = requests.post(addr, data=msg)
+
+    assert r.content == 'reply:' + msg
