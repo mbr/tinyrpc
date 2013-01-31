42b77fade2c1488d5b4551e0f917cec3cf401825
Marc Brinkmann <git@marcbrinkmann.de>
2013-01-31 23:52:46 +0100

    Moved some docs into sphinx rst files to avoid having to install optional dependencies to build docs.

--- tinyrpc/transports/zmq.py
+++ tinyrpc/transports/zmq.py
@@ -8,12 +8,6 @@ from . import ServerTransport, ClientTransport
 
 
 class ZmqServerTransport(ServerTransport):
-    """Server transport based on a :py:const:`zmq.ROUTER` socket.
-
-    :param socket: A :py:const:`zmq.ROUTER` socket instance, bound to an
-                   endpoint.
-    """
-
     def __init__(self, socket):
         self.socket = socket
 
@@ -26,29 +20,12 @@ class ZmqServerTransport(ServerTransport):
 
     @classmethod
     def create(cls, zmq_context, endpoint):
-        """Create new server transport.
-
-        Instead of creating the socket yourself, you can call this function and
-        merely pass the :py:class:`zmq.core.context.Context` instance.
-
-        By passing a context imported from :py:mod:`zmq.green`, you can use
-        green (gevent) 0mq sockets as well.
-
-        :param zmq_context: A 0mq context.
-        :param endpoint: The endpoint clients will connect to.
-        """
         socket = zmq_context.socket(zmq.ROUTER)
         socket.bind(endpoint)
         return cls(socket)
 
 
 class ZmqClientTransport(ClientTransport):
-    """Client transport based on a :py:const:`zmq.REQ` socket.
-
-    :param socket: A :py:const:`zmq.REQ` socket instance, connected to the
-                   server socket.
-    """
-
     def __init__(self, socket):
         self.socket = socket
 
@@ -60,17 +37,6 @@ class ZmqClientTransport(ClientTransport):
 
     @classmethod
     def create(cls, zmq_context, endpoint):
-        """Create new client transport.
-
-        Instead of creating the socket yourself, you can call this function and
-        merely pass the :py:class:`zmq.core.context.Context` instance.
-
-        By passing a context imported from :py:mod:`zmq.green`, you can use
-        green (gevent) 0mq sockets as well.
-
-        :param zmq_context: A 0mq context.
-        :param endpoint: The endpoint the server is bound to.
-        """
         socket = zmq_context.socket(zmq.REQ)
         socket.connect(endpoint)
         return cls(socket)
