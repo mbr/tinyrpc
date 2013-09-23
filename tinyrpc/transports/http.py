0310093ea77b6656c39ea157cd5ab2750df0741f
Sebastian Ortiz Vásquez <neoecos@gmail.com>
2013-09-23 18:10:20 -0500

    Added support for websocket in http client transport

--- tinyrpc/transports/http.py
+++ tinyrpc/transports/http.py
@@ -4,6 +4,7 @@
 from Queue import Queue
 import threading
 import requests
+import websocket
 
 from . import ServerTransport, ClientTransport
 
@@ -29,3 +30,34 @@ class HttpPostClientTransport(ClientTransport):
 
         if expect_reply:
             return r.content
+
+
+
+class HttpWebSocketClientTransport(ClientTransport):
+    """HTTP WebSocket based client transport.
+
+    Requires :py:mod:`websocket-python`. Submits messages to a server using the body of
+    an ``HTTP`` ``WebSocket`` message. Replies are taken from the response of the websocket.
+
+    The connection is establish on the ``__init__`` because the protocol is connection oriented,
+    you need to close the connection calling the close method.
+
+    :param endpoint: The URL to connect the websocket.
+    :param kwargs: Additional parameters for :py:func:`websocket.send`.
+    """
+    def __init__(self, endpoint, **kwargs):
+        self.endpoint = endpoint
+        self.request_kwargs = kwargs
+        self.ws = websocket.create_connection(self.endpoint, **kwargs)
+
+    def send_message(self, message, expect_reply=True):
+        if not isinstance(message, str):
+            raise TypeError('str expected')
+        self.ws.send(message)
+        r = self.ws.recv()
+        if expect_reply:
+            return r
+
+    def close(self):
+        if self.ws is not None:
+            self.ws.close()
