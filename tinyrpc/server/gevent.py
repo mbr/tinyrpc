67ef853a7775bf40570a30f6f66b0a0273de8b2b
crispamares <crispamares@gmail.com>
2013-10-26 16:56:07 +0200

    Add a start method to the gevent servers so you can run multiple servers and do a joinall for all of them.

--- tinyrpc/server/gevent.py
+++ tinyrpc/server/gevent.py
@@ -11,3 +11,10 @@ class RPCServerGreenlets(RPCServer):
     # documentation in docs because of dependencies
     def _spawn(self, func, *args, **kwargs):
         gevent.spawn(func, *args, **kwargs)
+
+    def start(self):
+        '''
+        Create a Greenlet with serve_forever so you can do a gevenet.joinall of 
+        several RPCServerGreenlets  
+        '''
+        return gevent.spawn(self.serve_forever)
