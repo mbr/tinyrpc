2f6b27beb1799344e7283040d4e9730fe7913a3d
lnoor <leo@noordergraaf.net>
2017-08-24 21:56:27 +0200

    set 0.6 version tag

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='tinyrpc',
-    version='0.6dev',
+    version='0.6',
     description='A small, modular, transport and protocol neutral RPC '
                 'library that, among other things, supports JSON-RPC and zmq.',
     long_description=read('README.rst'),
@@ -17,6 +17,8 @@ setup(
     keywords='json rpc json-rpc jsonrpc 0mq zmq zeromq',
     author='Marc Brinkmann',
     author_email='git@marcbrinkmann.de',
+    maintainer='Leo Noordergraaf',
+    maintainer_email='leo@noordergraaf.net',
     url='http://github.com/mbr/tinyrpc',
     license='MIT',
     install_requires=['six'],
