3f4c9c9d7c82e05c5f6b3403cff68271464f176d
lnoor <leo@noordergraaf.net>
2018-04-29 16:46:38 +0200

    create version 0.9

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='tinyrpc',
-    version='0.8',
+    version='0.9',
     description='A small, modular, transport and protocol neutral RPC '
                 'library that, among other things, supports JSON-RPC and zmq.',
     long_description=read('README.rst'),
